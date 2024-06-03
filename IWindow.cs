e2da687fc3e9ba948d1809705dbaf414bb2889c6
Кітаєва Катерина <kateruna1502@gmail.com>
2024-06-03 08:22:56 +0300

    win.cs

---
+++ IWindow.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ShortestPathSolver
+{
+    interface IWindow
+    {
+        void TopPanel_MouseDown(object sender, MouseEventArgs e);
+        void TopPanel_MouseMove(object sender, MouseEventArgs e);
+        void ButtonToExit_Click(object sender, EventArgs e);
+    }
+}
